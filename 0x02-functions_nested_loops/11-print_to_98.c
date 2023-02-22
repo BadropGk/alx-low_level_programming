cac434c6bc4ac013ed6dd7aa184c90b9dd84e7e7
BadropGk <otienockras001@gmail.com>
2023-02-22 11:32:15 -0800

    c11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,45 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* add - Adds two values
-* @n1: The first number to be computed
-* @n2: The second number to be computed
-* Return: Integer value
+* print_to_98 - Prints all natural numbers from n to 98
+* @n: The number to start printing from
+* Return: Always 0.
 */
+
 void print_to_98(int n)
+
 {
 if (n <= 98)
 {
 for (; n <= 98; n++)
 {
-printf("%d", n);
 if (n == 98)
-continue;
-printf(", ");
-}
-printf("\n");
-}
-else
-}
-for (; n >= 98; n--)
 {
 printf("%d", n);
-if (n ==   98)
-continue;
-printf(", ");
-}
 printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
 }
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
-int print_last_digit(int r)
+* print_last_digit - prints the last digit of a number
+* @n: The number to be treated
+* Return: Value of the last digit of number
+*/
+
+int print_last_digit(int n)
 {
-int n;
-if (r < 0)
-n = -1 * (r % 10);
-else
-n = r % 10;
-_putchar((n % 10) + '0');
-return (n % 10);
+int last;
+last = n % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
 }
